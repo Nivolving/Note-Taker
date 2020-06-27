221a2eccea8f7da39391133530831f3293590117
Nivolving <nivethapartheeban@gmail.com>
2020-06-27 09:23:50 -0700

    root structure changed

---
+++ db/store.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
+const { v1 : uuidv1 } = require('uuid');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const dbPath = path.join(__dirname,"../db/db.json");
+
+class Store {
+    read()
+    {
+        return readFileAsync(dbPath, "utf8");
+    }
+
+    write( content )
+    {
+        return writeFileAsync( dbPath, content );
+
+    }
+
+    getNotes()
+
+    {
+        return this.read().then((data) => {
+
+            return data 
+
+            ? JSON.parse ( data ) 
+
+            : [];
+         });
+       
+    }
+
+    addNotes( note )
+    {
+        return this.getNotes().then((notes) =>
+        {
+            const newNote = { ...note, id:uuidv1() };
+            notes.push ( newNote );
+
+            
+
+            return this.saveNotes ( notes ).then(() => newNote);
+
+        });
+
+    }
+
+    saveNotes( notes ){
+
+        return this.write ( JSON.stringify( notes ));
+
+    }
+    deleteNote( noteId ){
+
+        return this
+        .getNotes()
+        .then( (notes) => {
+
+            const newList = notes.filter( (note) => note.id !== noteId);
+            
+            this.saveNotes(newList);
+        });
+
+    }
+
+}
+
+module.exports = new Store;
---
+++ server.js
@@ -0,0 +1,19 @@
+//require the express module
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes")
+//create a server
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use( express.static('public') );
+app.use("/api",apiRoutes );
+app.use( htmlRoutes );
+
+//define the port
+const PORT = process.env.PORT || 3000;
+
+app.listen( PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
